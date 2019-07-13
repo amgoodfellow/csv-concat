17ec4eb5e46fa97062ec1189af7478d6728ff3e4
Aaron Goodfellow <amgoodfellow@oakland.edu>
2019-07-13 15:44:58 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+extern crate glob;
+use glob::glob;
+use std::fs::{File, OpenOptions};
+use std::io::{BufRead, BufReader, Result, Write};
+
+fn main() -> Result<()> {
+    let dest_file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open("all.csv")?;
+
+    let mut header_inserted = false;
+    for entry in glob("./*.csv").expect("Invalid pattern") {
+        match entry {
+            Ok(path) => {
+                let file_name = path.clone();
+                let file = File::open(path)?;
+                let mut line_number = 0;
+                for line in BufReader::new(file).lines() {
+                    if header_inserted == false && line_number == 0 {
+                        writeln!(&dest_file, "File,{}", line?)?;
+                        header_inserted = true;
+                    } else {
+                        writeln!(&dest_file, "{},{}", file_name.display(), line?)?;
+                    }
+                    line_number += 1;
+                }
+            }
+            Err(e) => println!("{:?}", e),
+        }
+    }
+    Ok(())
+}
